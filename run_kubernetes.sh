5d89ee952e6fd17e4e2d57b82c240a3c0fee5d98
Kelani Tolulope <kelanik8@gmail.com>
2021-02-16 16:24:36 +0100

    config updates

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 dockerpath=kelanik8/capstone_project
-TAG=capstone_project
+TAG=capstoneproject
 
 kubectl run $TAG --image=$dockerpath --generator=run-pod/v1
 
 kubectl get pods
 
-kubectl port-forward pod/$TAG 8000:80
+kubectl port-forward pod/$TAG 8001:80
